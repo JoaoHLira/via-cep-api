7f12aceeb06e4118c82d114e0cc70efe9405184c
JoaoHLira <lirarosas.jh@gmail.com>
2024-12-06 15:32:27 -0300

    adiciona logs

--- src/main/java/br/com/joaolirarosas/via_cep/endereco/api/CepController.java
+++ src/main/java/br/com/joaolirarosas/via_cep/endereco/api/CepController.java
@@ -2,12 +2,14 @@ package br.com.joaolirarosas.via_cep.endereco.api;
 
 import br.com.joaolirarosas.via_cep.endereco.domain.Endereco;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@Log4j2
 public class CepController {
 
     private final CepFeignService cepFeignService;
@@ -15,7 +17,9 @@ public class CepController {
     @GetMapping("/{cep}")
     @ResponseStatus(code = HttpStatus.OK)
     public Endereco getEnderecoByCEP(@PathVariable("cep") String cep) {
+        log.info("[start] CepController - getEnderecoByCEP");
         EnderecoDTO endereco = cepFeignService.buscaEnderecoPorCEP(cep);
+        log.debug("[finish] CepController - getEnderecoByCEP");
         return new Endereco(endereco);
     }
 }
